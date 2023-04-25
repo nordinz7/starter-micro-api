dbab2ce7f611cec53cb96293875e1ea7b0d17eb6
nordin <nordin.zahari@swiftlogistics.com.my>
2023-04-25 21:46:32 +0800

    added js doc

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const { getPrayerTimes } = require('./utils')
 
+/**
+ * Description
+ * @param {Period} 'refer to utils/Period.js values'
+ * @param {string} 'refer to file: zone.json values'
+ * @returns {
+ * prayerTime: Array
+ * status: string
+ * serverTime: string
+ * periodType: string
+ * lang: string
+ * zone: string
+ * bearing: string
+ * }
+ */
+
 getPrayerTimes('today', 'SGR03')
